893733c6a103a1b00cea84101c3017902feb7421
Dawiczku <skarupski.dawid@gmail.com>
2022-12-12 22:00:13 +0100

    Add function that evokes right equations

--- main.js
+++ main.js
@@ -15,3 +15,24 @@ function multiplication(number1, number2) {
 function division(number1, number2) {
     number2 !== 0 ? number1 / number2 : 'Error';
 }
+
+function operate(number1, operator, number2) {
+    switch(operator) {
+        case '+': 
+            result = addition(number1, number2);
+            break;
+        case '-':
+            result = substraction(number1, number2);
+            break;
+        case '*':
+            result = multiplication(number1, number2);
+            break;
+        case '/':
+            result = multiplication(number1, number2);
+            break;
+    }
+}
+
+// Variables
+
+let result = null;
