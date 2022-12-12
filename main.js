7642f634a58b3df6c8442815c0c4f2f1db5dc17a
Dawiczku <skarupski.dawid@gmail.com>
2022-12-12 21:51:17 +0100

    Add functions for basic math equasions

---
+++ main.js
@@ -0,0 +1,17 @@
+// Functions
+
+function addition(number1, number2) {
+    return number1 + number2;
+}
+
+function substraction(number1, number2) {
+    return number1 - number2;
+}
+
+function multiplication(number1, number2) {
+    return number1 * number2;
+}
+
+function division(number1, number2) {
+    number2 !== 0 ? number1 / number2 : 'Error';
+}
