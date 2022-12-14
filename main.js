47fbd5d82bc3ae5adceaaca3f73fade9abe127d7
Dawiczku <skarupski.dawid@gmail.com>
2022-12-15 00:15:13 +0100

    Add functionality to operation buttons

--- main.js
+++ main.js
@@ -19,38 +19,55 @@ function division(number1, number2) {
 }
 
 function operate(number1, operator, number2) {
+
+    let result = null;
+
     switch(operator) {
         case '+': 
-            operationResult = addition(number1, number2);
-            break;
+            result = addition(number1, number2);
+            return result;
         case '-':
-            operationResult = substraction(number1, number2);
-            break;
+            result = substraction(number1, number2);
+            return result;
         case '*':
-            operationResult = multiplication(number1, number2);
-            break;
+            result = multiplication(number1, number2);
+            return result;
         case '/':
-            operationResult = multiplication(number1, number2);
-            break;
+            result = division(number1, number2);
+            return result;
     }
 }
 
 function updateMainDisplay(value) {
-    currentValueDisplay.textContent = value;
+    if(Number.isInteger(Number(value))) {
+        currentValueDisplay.textContent = value;
+    } else {
+        currentValueDisplay.textContent = Number(value).toFixed(2).toString();
+    }
+}
+
+function updateSmallDisplay(value, operation) {
+    if(Number.isInteger(Number(value))) {
+        lastValueDisplay.textContent = `${value} ${operation}`;
+    } else {
+        lastValueDisplay.textContent = `${Number(value).toFixed(2).toString()} ${operation}`;
+    }
 }
 
 // Variables
 
-let operationResult = null;
 let currentOperation = null;
-let currentStringValue = "";
-let firstNumber = null;
+let firstStringValue = "";
+let secondStringValue = "";
+let firstValue = null;
+let secondValue = null;
 
 // DOM elements and methods
 
 const numberButtons = Array.from(document.getElementsByClassName("number"));
 const operationButtons = Array.from(document.getElementsByClassName("operation"));
 let currentValueDisplay = document.getElementById("current-calc");
+let lastValueDisplay = document.getElementById("last-calc");
 
 // ----- Main section -----
 
@@ -61,15 +78,21 @@ for(let number of numberButtons) {
     number.addEventListener("click", () => {
 
         // Make sure, zero would not overlap
-        if(Number(number.value) === 0 && currentStringValue == ""){
+        if(Number(number.value) === 0 && firstStringValue == ""){
             return;
+        // Writing value to second string if first one is not empty and operation is choosed
+        } else if(firstStringValue != "" && currentOperation != null) {
+            secondStringValue += number.value;
+        
         } else {
-            currentStringValue += number.value;
+            firstStringValue += number.value;
         }
 
         // Checking if the number is not too large, if so setting maximum value.
-        if(currentStringValue.length < 10) {
-            updateMainDisplay(currentStringValue);
+        if(firstStringValue.length < 10 && secondStringValue == "") {
+            updateMainDisplay(firstStringValue);
+        } else if(secondStringValue.length < 10 && secondStringValue != "") {
+            updateMainDisplay(secondStringValue);
         } else {
             return;
         }
@@ -78,6 +101,31 @@ for(let number of numberButtons) {
 
 for(let operation of operationButtons) {
     operation.addEventListener("click", () => {
-        currentOperation = operation.value;
+        // If second value is not present, you can change the operation sign
+        if(secondStringValue == "") {   
+            currentOperation = operation.value;
+            firstValue = Number(firstStringValue);
+            updateSmallDisplay(firstValue, currentOperation);
+            updateMainDisplay("");
+        /* Else if the second value is present and user clicks operation button 
+           again, two previous values get calculated*/
+        } else {
+            firstValue = Number(firstStringValue);
+            secondValue = Number(secondStringValue);
+            let operationResult = operate(firstValue, currentOperation, secondValue);
+            updateMainDisplay("");
+            if(operation.value == '=') {
+                lastValueDisplay.textContent = `${firstValue} ${currentOperation} ${secondValue} =`;
+                updateMainDisplay(operationResult);
+                currentOperation = null;
+            } else {
+                currentOperation = operation.value;
+                updateSmallDisplay(operationResult, currentOperation);
+            }
+            firstStringValue = operationResult;
+            secondStringValue = "";
+            firstValue = null;
+            secondValue = null;
+        }
     })
 }
